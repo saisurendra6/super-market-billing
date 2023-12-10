30412e658ac6cf2115f92458ca285f529c6653c7
Sai Surendra <saisurendra.kusam@gmail.com>
2023-12-10 10:50:56 +0530

    inital commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello Welcome to MANA Super Market :-)");
+        int choice = 0;
+        while (choice != 4) {
+            System.out.println("1. Go to shop\n2. Cutomer Details\n3. Store Data\n4.Exit\nEnter your choice:");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("directing u to shoping...");
+                    break;
+                case 2:
+                    System.out.println("directing u to customer details...");
+                    break;
+                case 3:
+                    System.out.println("directing u to store data...");
+                    break;
+                case 4:
+                    System.out.println("exiting...");
+                    break;
+
+                default:
+                    System.out.println("enter a valid choice");
+                    break;
+            }
+        }
     }
 }
---
+++ src/shopping/BillItem.java
@@ -0,0 +1,59 @@
+package shopping;
+
+public class BillItem {
+    String name;
+    String code;
+    float MRP;
+    int qty;
+    float discount;
+    float totalVal;
+
+    public BillItem(String name, String code, float mRP, int qty, float discount) {
+        this.name = name;
+        this.code = code;
+        MRP = mRP;
+        this.qty = qty;
+        this.discount = discount;
+        getTotalVal();
+    }
+
+    public BillItem(BillItem item){
+        this.name = item.name;
+        this.code = item.code;
+        MRP = item.MRP;
+        this.qty = item.qty;
+        this.discount = item.discount;
+        getTotalVal();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getCode() {
+        return code;
+    }
+    public float getMRP() {
+        return MRP;
+    }
+    public float getDiscount() {
+        return discount;
+    }
+    public int getQty() {
+        return qty;
+    }
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getTotalVal() {
+        totalVal = MRP * (qty - discount / 100);
+        return totalVal;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + " code: " + code + " mrp: " + MRP + " qty: " + qty + " discount: " + discount
+                + " tot: " + totalVal;
+    }
+
+}
